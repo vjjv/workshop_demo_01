f74c726f59d79bb8cac2a94e66ed62b17f37e67b
Bastien Sarolea <bastien.sarolea@gmail.com>
2023-10-12 18:00:51 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ import {
     const source = createMediaStreamSource(mediaStream, {
         // transform: Transform2D.MirrorX,
         cameraType: 'back'
-    })
+    });
 
     await session.setSource(source)
 
